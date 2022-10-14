359d782d41e5b6a0c272b4093a78bf3653984a0a
nour khoury <khourynour79@gmail.com>
2022-10-14 15:02:37 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import BlogContent from "./BlogContent";
 import BlogPost from "./BlogPost";
 
 function App() {
-  return <BlogPost />;
+  return (
+  <>
+  <BlogContent articleText="React Focus On UI" num={2}/>
+  <BlogContent articleText="React is created By Facebook"/>
+  <BlogPost/>
+  </>
+  );
 }
 
 export default App;
--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  return <div id="blog-content">{props.articleText} NUM {props.num}</div>;
 }
 
 export default BlogContent;
